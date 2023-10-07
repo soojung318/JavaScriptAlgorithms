1dc1639acd3645fad53b4449c164d896da3b3b04
Chris <sjshin0318@gmail.com>
2023-10-07 15:50:05 +0900

    2. 삼각형 판별하기

---
+++ Algorithms/2.js
@@ -0,0 +1,24 @@
+function solution(a,b,c){
+    let maxNum,num, answer;
+    if(a>b) {
+        maxNum = a;
+        num = b + c;
+    }
+    maxNum = b;
+    num = 0;
+    num = a + c;
+    if(c > maxNum) {
+        maxNum = c;
+        num = 0;
+        num = a + b;
+    }
+
+    if (maxNum < num){
+        answer = "Yes";
+    } else {
+        answer = "No";
+    }
+    return answer;
+}
+
+console.log(solution(13, 33, 17));
