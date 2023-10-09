1bb6406c8744735940ce6bf9b2263590460d47ec
Chris <sjshin0318@gmail.com>
2023-10-09 23:08:52 +0900

    6. 홀수

--- Algorithms/6.js
+++ Algorithms/6.js
@@ -1,10 +1,10 @@
-function solution(arr){
-    let answer=[];
-    let sum=0, min = Number.MAX_SAFE_INTEGER;
-    for (let i of arr){
-        if(i%2===1) {
-            sum+=i;
-            if(min>i) min=i;
+function solution(arr) {
+    let answer = [];
+    let sum = 0, min = Number.MAX_SAFE_INTEGER;
+    for (let i of arr) {
+        if (i % 2 === 1) {
+            sum += i;
+            if (min > i) min = i;
         }
         answer.push(sum);
         answer.push(min);
@@ -13,5 +13,5 @@ function solution(arr){
 
 }
 
-let arr = [12, 77, 38, 41, ,53, 92, 85];
+let arr = [12, 77, 38, 41, , 53, 92, 85];
 console.log(solution(arr)); //[0, 9007199254740991]
