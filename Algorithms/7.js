51eff1369f4795777d5d0dd9da164028bdfdb067
Soojung Shin <sjshin0318@gmail.com>
2023-10-11 14:50:41 +0900

    10. 10부제

---
+++ Algorithms/7.js
@@ -0,0 +1,10 @@
+function solution(date, arr){
+    let answer=0;
+    for (let i of arr){
+        if (i%10 === date) answer++;
+    }
+    return answer;
+}
+
+let arr = [25, 23, 11, 47, 53, 17, 33];
+console.log(solution(3,arr));
