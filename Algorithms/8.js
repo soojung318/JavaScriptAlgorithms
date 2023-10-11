af999ec4711a28834ae5523390dedc702980401e
Soojung Shin <sjshin0318@gmail.com>
2023-10-11 15:52:07 +0900

    8. 일곱난쟁이

--- Algorithms/8.js
+++ Algorithms/8.js
@@ -3,11 +3,14 @@ function solution(arr){
     let sum = arr.reduce((a,b)=>a+b, 0);
     for(let i=0; i<8; i++){
         for(let j=i+1; j<9; j++){
-            if((sum-(arr[i]+arr[j]))===100){
+            if((sum-(arr[i]+arr[j]))==100){
                 arr.splice(i,1);
                 arr.splice(j,1);
+                flag=1;
+                break;
             }
         }
+        if(flag==1) break;
     }
 
     return answer;  //[20, 23, 19, 10, 15, 8]
