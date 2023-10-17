1f7382560236681d4570d172e7f4065f0d07bb51
Soojung Shin <sjshin0318@gmail.com>
2023-10-17 16:01:00 +0900

    기본문법-프로미스 객체

--- Syntax/PromiseObject.js
+++ Syntax/PromiseObject.js
@@ -79,10 +79,52 @@ workA(10, (resA) => {
             console.log(`3. ${resC}`);
         });
     });
-});
+}); // >> 비동기함수는 이러한 콜백지옥에 빠질 수 있음 이걸 "프로미스 객체"로 해결한다.
 
 workD("workD", (res) => {
     console.log(res);
 });
 // 비동기 함수의 결과값을 계속해서 전달애햐하기 때문에 순서대로 실행되어
 // 10에 5를 더한 15, 15에 3을 뺀 12,12에 10을 더한 22가 출력되는 것을 볼 수 있다.
+
+
+/*
+    프로미스 객체 : 비동기 함수를 더 편하게 이용할 수 있게 하는 자바스크립트 내장객체. 인수로 executor라는 실행함수를 전달한다.
+    resolve, reject는 자바스크립트에서 자체적으로 제공하는 콜백함수로, executor이 비동기처리가 성공하면 resolve를,
+    비동기처리가 실패한다면 reject를 호출한다. 비동기 처리 작업은 항상 성공할 수도, 실패할 수도 있기 때문에
+    (resolve, reject) 둘 중 하나는 반드시 호출해야 한다.
+
+    프로미스는 객체이기 때문에 프로퍼티들을 가지고 있다.
+    프로미스 객체의 내부 프로퍼티 : (state, result)
+    프로미스 객체는 맨 처음 대기상태와 Undefined 값을 가지고 있다가 executor가 호출하는 콜백함수에 따라 State와 Result가 변화된다.
+
+   [ new Promise(executor) ]
+     state: pending(대기)    -----resolve(value)----->  state: fulfilled(성공)
+     result: undefined                                  result: value
+
+          "                  ----reject(error)------>   state: rejected(실패)
+                                                        result: error
+
+    이렇게 프로미스 객체의 상태는 resolve와 reject를 통해 변하지만, 한번 변경된 상태에서는 더이상 변하지 않기 때문에
+    처리가 끝난 프로미스 객체에 resolve 또는 reject를 호출하면 무시된다는 것을 주의해야 한다.
+*/
+
+// resolve,reject 호출될 때 Promise 내부 property 값이 어떻게 변화하는지 보는 예제
+const executor = (resolve, reject) => {
+    setTimeout(() => {
+        console.log("3초만 기다리세요");
+    }, 5000);
+};
+const promise = new Promise(executor);
+
+// resolve, reject 사용법 예제
+const executor = (resolve, reject) => {
+    setTimeout(() => {
+        resolve("성공"); // 1. 이 resolve 콜백함수에 전달된 값은
+    }, 5000);
+};
+const promise = new Promise(executor);
+promise.then((res) =>{ //2. promise 객체의 then 메서드를 이용해 사용할 수 있고, 이 then 메서드는 executor 함수에서 전달한 값이 매개변수로 전달된다.
+    console.log(res);  //3. 이 executor에서 전달한 성공이라는 값을 콜백함수의 매개변수로 전달받을 수 있다.
+}); //3초후 "성공" 출력.
+
